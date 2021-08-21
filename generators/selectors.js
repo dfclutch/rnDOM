ef5b352569ed8eb3c4174bbc1b973d6ccf6d9671
Dan Filler <dwfiller@gmail.com>
2021-08-21 15:15:52 -0400

    better array typechecking

--- generators/selectors.js
+++ generators/selectors.js
@@ -1,19 +1,33 @@
 const { intBetween } = require('./numbers');
 
-const oneOf = (a) => a[intBetween(0, a.length)];
+const oneOf = (a) => {
+  if (!Array.isArray(a)) {
+    throw new Error('rndom: Argument must be an array');
+  }
+
+  return a[intBetween(0, a.length)];
+};
 
 const nOf = (n, a) => {
+  if (!Array.isArray(a)) {
+    throw new Error('rndom: Argument must be an array');
+  }
+
+  if (n > a.length) {
+    throw new Error(`Cannot select ${n} elements from an array of length ${a.length}`);
+  }
+
   const indices = [];
-  const output = [];
+  const selectedValues = [];
   while (indices.length < n) {
-    const candidate = intBetween(0, a.length);
-    if (!indices.includes(candidate)) {
-      indices.push(candidate);
-      output.push(a[candidate]);
+    const candidateIndex = intBetween(0, a.length);
+    if (!indices.includes(candidateIndex)) {
+      indices.push(candidateIndex);
+      selectedValues.push(a[candidateIndex]);
     }
   }
 
-  return output;
+  return selectedValues;
 };
 
 module.exports = { oneOf, nOf };
